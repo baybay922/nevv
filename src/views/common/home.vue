1c6c33db07c1ed925a8e7ef8f4d595f4f3cdec49
bay <bb.wu@ukuoffer.com>
2020-11-06 20:33:18 +0800

    首次更新

---
+++ src/views/common/home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="mod-home">
+      你好
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style>
+  .mod-home {
+    line-height: 1.5;
+  }
+</style>
+
